d3f0c6875316fc6ca5117adda4cf23a918795dc4
Christopher Schultz <chris@christopherschultz.net>
2026-03-15 12:24:44 -0400

    Remove trailing whitespace
    No functional change

--- Leaads2026/main.py
+++ Leaads2026/main.py
@@ -14,7 +14,7 @@ red: tuple[int, int, int] = (255, 0, 0)
 
 orange: tuple[int, int, int] = (255, 50, 0)
 
-yellow = (255, 255, 0) 
+yellow = (255, 255, 0)
 yelreen = (255, 215, 0)
 
 green = (0, 255, 0)
@@ -58,12 +58,12 @@ class Animator:
         self.sleep_duration = 1.0
 
     def set_animation(self, frames: list[list[tuple[int,int,int]]], sleep_duration: float):
-        
+
         if self.frames != frames:
             self.current_frame_idx = 0
             self.frames = frames
         self.sleep_duration = sleep_duration
-        
+
     def get_current_frame(self):
         return self.frames[self.current_frame_idx]
 
@@ -79,7 +79,7 @@ class Animator:
                 print("Writing frame " + str(self.current_frame_idx) + " / " + str(frame_count))
                 self.pixels.write()
 		self.current_frame_idx += 1
-            
+
             if frame_count > 1:
                 print("Sleeping for " + str(self.sleep_duration))
                 sleep(self.sleep_duration)
@@ -95,78 +95,78 @@ def interpolate(start_color: tuple[int, int, int], end_color: tuple[int, int, in
     red_slope = (end_color[0] - start_color[0])/steps
     green_slope = (end_color[1] - start_color[1])/steps
     blue_slope = (end_color[2] - start_color[2])/steps
-    
+
     for indx in range(steps - 2):
         result_indx = indx + 1
         result_red = red_slope * result_indx + start_color[0]
         result_green = green_slope * result_indx + start_color[1]
         result_blue = blue_slope * result_indx + start_color[2]
-        result[result_indx] = (int(result_red), int(result_green), int(result_blue))  
-    
+        result[result_indx] = (int(result_red), int(result_green), int(result_blue))
+
     return result
 
 def all_color(color: tuple[int, int, int]) -> list[list[tuple[int, int, int]]]:
     result = []
     frame: list[tuple[int, int, int]] = [color]*num_leds
     result.append(frame)
-    
+
     return result
 
 def all_off():
-    return all_color(off)       
+    return all_color(off)
 
 def dueling_serpents():
     result =[]
     mid_state = []
-    
+
     for i in range(0, halfway + 1):
-        
+
         frame = [off]*num_leds
-        
+
         for j in range(0, i):
             frame[j] = blue
             frame[-1 - j] = red
-            
+
         result.append(frame)
-        
+
         mid_state = frame
-        
+
     for i in range(halfway, -1, -1):
         frame = mid_state.copy()
-        
+
         for j in range(halfway, i - 1, -1):
             frame[-1 - j] = blue
             frame[j] = red
-        
+
         result.append(frame)
-    
-    return result 
-        
+
+    return result
+
 def teal_orange_purple():
     result = []
     frame = [off] * num_leds
     frame[0] = teal
     frame[halfway] = orange
     frame[-1] = purple
-    
+
     first_half: list[tuple[int, int, int]] = interpolate(frame[0], frame[halfway], halfway)
     second_half = interpolate(frame[halfway], frame[-1], halfway)
-    
+
     for idx, led_color in enumerate(first_half):
         frame[idx] = led_color
-        
+
     for idx, led_color in enumerate(second_half):
         frame[idx + halfway] = led_color
-        
+
     result.append(frame)
-    
-    cur_frame = frame.copy()    
+
+    cur_frame = frame.copy()
     for _ in range(num_leds - 1):
         cur_frame.insert(0, cur_frame.pop(-1))
         result.append(cur_frame)
         cur_frame = cur_frame.copy()
-        
-    
+
+
     return result
 
 def rainbow_gradient_long_run() -> list[list[tuple[int, int, int]]]:
@@ -175,58 +175,58 @@ def rainbow_gradient_long_run() -> list[list[tuple[int, int, int]]]:
     frame[0] = green
     frame[halfway] = red
     frame[-1] = teal
-    
+
     first_half: list[tuple[int, int, int]] = interpolate(frame[0], frame[halfway], halfway)
     second_half = interpolate(frame[halfway], frame[-1], halfway)
-    
+
     for idx, led_color in enumerate(first_half):
         frame[idx] = led_color
-        
+
     for idx, led_color in enumerate(second_half):
         frame[idx + halfway] = led_color
-        
+
     result.append(frame)
-    
-    cur_frame = frame.copy()    
+
+    cur_frame = frame.copy()
     for _ in range(num_leds - 1):
         cur_frame.insert(0, cur_frame.pop(-1))
         result.append(cur_frame)
         cur_frame = cur_frame.copy()
-        
-    
+
+
     return result
 
 def orange_purple_gradient_long() -> list[list[tuple[int, int, int]]]:
 
     frame: list[tuple[int, int, int]] = interpolate(orange, purple, num_leds)
-        
+
     return [frame]
 
 def breathe_orange_purple():
     result = []
-    
+
     almost_off_orange = (int(orange[0]/20), int(orange[1]/20), int(orange[2]/20))
     almost_off_purple = (int(purple[0]/20), int(purple[1]/20), int(purple[2]/20))
 
     breathe_orange: list[tuple[int, int, int]] = interpolate(almost_off_orange, orange, 15)
     breathe_purple: list[tuple[int, int, int]] = interpolate(almost_off_purple, purple, 15)
-    
+
     for my_color in breathe_orange:
         result.append([my_color]*num_leds)
-    
+
     breathe_orange.reverse()
-    
+
     for my_color in breathe_orange:
         result.append([my_color]*num_leds)
-        
+
     for my_color in breathe_purple:
         result.append([my_color]*num_leds)
-    
+
     breathe_purple.reverse()
-    
+
     for my_color in breathe_purple:
         result.append([my_color]*num_leds)
-    
+
     return result
 
 def tada():
@@ -237,24 +237,24 @@ def tada():
         green, pink, fuchsia, yellow, white, light_blue,
         neon_pink, light_purple, orange, steel_gray, red, sea_foam
 ]
-    
+
     first_explosion = my_colors.copy()
-    
-    second_explosion = my_colors.copy()    
+
+    second_explosion = my_colors.copy()
     second_explosion.reverse()
-    
+
     for i in range(0, halfway):
-        
+
         frame = [off]*num_leds
-        
+
         for j in range(0, i):
             frame[j] = purple
             frame[-1 - j] = orange
-            
+
         result.append(frame)
-    
+
     explosion = first_explosion + second_explosion
-    
+
     for _ in range(5):
         result.append(explosion[0:num_leds])
         result.append([off]*num_leds)
@@ -263,12 +263,12 @@ def tada():
     first_explosion.reverse()
     second_explosion = my_colors.copy()
     explosion = first_explosion + second_explosion
-    
+
     for _ in range(5):
         result.append(explosion[0:num_leds])
         result.append([off]*num_leds)
 
-    
+
     return result
 
 ## Lambda which selects a single frame from an animation, for debugging
@@ -340,7 +340,7 @@ try:
 
     while True:
         read_key = uart.read(1)
-        
+
         if(read_key is not None and read_key in patterns):
             robot_key = read_key
 
@@ -353,7 +353,7 @@ try:
             input()
 
         animator.advance()
-        
+
 finally:
     neo_out.fill(off)
     neo_out.write()
